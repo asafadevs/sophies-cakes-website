3b36e3ef1a94cc73d6a709edc2e18a6230d9b398
Asafa Makan'a <asafajr96@gmail.com>
2020-11-18 16:26:21 +0200

    order & contact forms

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 const t1 = gsap.timeline({defaults:{ease: 'power.out'}});
 
-t1.to(".text", {y: "0%", duration: 1.5, stagger: 0.416});
+t1.to(".text", {y: "0%", duration: 1.2, stagger: 0.416});
 t1.to(".slider", {y: "-100%", duration: 1.5, delay: 0.5});
 t1.to(".animation", {y: "-200%", duration: 1}, "-=1");
 t1.to(".nav-menu", {y:"0%", duration: 0.5})
---
+++ js/pagetrans.js
@@ -0,0 +1,5 @@
+const t1 = gsap.timeline({defaults:{ease: 'power.out'}});
+
+t1.to(".transition", {x: "100%", duration: 1.5});
+t1.fromTo(".transition", {opacity: 1}, {opacity: 0, duration: .1});
+t1.to(".transition", {x: "-200%", duration: 1.5});
