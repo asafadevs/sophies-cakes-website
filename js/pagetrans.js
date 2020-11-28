1d9ccfc58bb75a8df0f9816951a395b1ccb30272
Asafa Makan'a <asafajr96@gmail.com>
2020-11-28 20:49:12 +0200

    mobile view fix 1

--- js/pagetrans.js
+++ js/pagetrans.js
@@ -1,6 +1,3 @@
 const t1 = gsap.timeline({defaults:{ease: 'power.out'}});
 
-t1.to(".transition", {x: "100%", duration: 1.5, delay: .5});
-t1.fromTo(".transition", {opacity: 1}, {opacity: 0, duration: .1});
-t1.to(".transition", {x: "-200%", duration: .1});
 t1.to(".nav-menu", {y:"0%", duration: 0.5})
