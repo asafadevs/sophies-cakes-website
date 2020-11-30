ccf4414426d3cf832bfc213e5088d85464c4e00e
Asafa Makan'a <asafajr96@gmail.com>
2020-11-30 20:58:33 +0200

    contact page mobiel view edit

--- js/pagetrans.js
+++ js/pagetrans.js
@@ -1,3 +1,3 @@
 const t1 = gsap.timeline({defaults:{ease: 'power.out'}});
-
-t1.to(".nav-menu", {y:"0%", duration: 0.5})
+t1.to(".transition", {y:"-110%", duration: 1.5, delay: 1});
+t1.to(".nav-menu", {y:"0%", duration: 0.5});
